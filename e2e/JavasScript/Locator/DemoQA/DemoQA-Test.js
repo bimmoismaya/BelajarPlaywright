cae3db71f1fe3ca3eb49c17f32f61a7c918ff30e
bimmo-ismaya <ismaya.bimmo@gmail.comgit config --global user.email ismaya.bimmo@gmail.comgit status>
2025-11-04 20:27:10 +0700

    first-checkbox-push

--- e2e/JavasScript/Locator/DemoQA/DemoQA-Test.js
+++ e2e/JavasScript/Locator/DemoQA/DemoQA-Test.js
@@ -13,20 +13,21 @@ export class GoToElementPageDemo {
     }
 
     async gotoElementPage() {
-        await this.page.goto(this.url);
+        await this.page.goto(this.url, {timeout: 45000});
         await this.elementButton.click();
     }
 }
 
-/** masuk ke page elements
+/** masuk ke page Text
  * 
  */
 
-export class TextBoxElementDemo {
+export class TextBoxTextDemo {
     constructor(page) {
         this.page = page;
-        this.urlElements = 'https://demoqa.com/elements';
         this.buttonTextBox = page.locator('li:has-text("Text Box")');
+        this.urlElementPage = 'https://demoqa.com/elements';
+        this.urlTextBox = 'https://demoqa.com/text-box';
         this.UserName = page.getByRole('textbox', { name: 'Full Name' })
         this.UserEmail = page.locator('#userEmail');
         this.currentAddress = page.locator('#currentAddress');
@@ -39,42 +40,101 @@ export class TextBoxElementDemo {
         this.resultPermanentAddress = page.locator("//p[@id='permanentAddress']");
     }
 
-    
-
-    async expectSuccessGoToElementsPage(){
-        await expect(this.page).toHaveURL(this.urlElements);
-        // await expect(this.logoTextBox).toContainText('Text Box');
-    }
-
-    async toTextBoxPage(){
+    async gotoTextBoxPage() {
         await this.buttonTextBox.click();
     }
 
-    async inputDataInfo(username, email, current, permanent){
+    async expectSuccessGoToElementsPage() {
+        await expect(this.page).toHaveURL(this.urlElementPage);
+    }
+
+    async expectSuccessGoToTextBoxPage() {
+        await expect(this.page).toHaveURL(this.urlTextBox);
+    }
+
+
+    async inputDataInfo(username, email, current, permanent) {
         await this.UserName.fill(username);
         await this.UserEmail.fill(email);
         await this.currentAddress.fill(current);
         await this.permanentAddress.fill(permanent);
     }
 
-    async hitButtonSubmit(){
+    async hitButtonSubmit() {
         await this.submitButtoon.click();
     }
-/**
- * penggunaan toContainText dibawah ini adalah untuk mengambil semua data yang ada dilocator
- * bisa menggunakan toHaveText, namun harus menambahkan sedikit kode menjadi seperti ini
- * toHaveText(`Name:${username}`);
- * toHaveText(`Email:${email}`);
- * toHaveText(`Current Address :${current}`);
- * toHaveText(`Permananet Address :${permanent}`);
- * 
- * jika mau menggunakan toHaveText, diluar itu bisa gunakan toContainText()
- */
+    /**
+     * penggunaan toContainText dibawah ini adalah untuk mengambil semua data yang ada dilocator
+     * bisa menggunakan toHaveText, namun harus menambahkan sedikit kode menjadi seperti ini
+     * toHaveText(`Name:${username}`);
+     * toHaveText(`Email:${email}`);
+     * toHaveText(`Current Address :${current}`);
+     * toHaveText(`Permananet Address :${permanent}`);
+     * 
+     * jika mau menggunakan toHaveText, diluar itu bisa gunakan toContainText()
+     */
 
-    async expectedResultSubmit(username, email, current, permanent){
+    async expectedResultSubmit(username, email, current, permanent) {
         await expect(this.resultName).toContainText(username);
         await expect(this.resultEmail).toContainText(email);
         await expect(this.resultCurrentAddress).toContainText(current);
         await expect(this.resultPermanentAddress).toContainText(permanent);
     }
+}
+
+export class CheckBoxPage {
+    constructor(page) {
+        this.page = page;
+        this.urlElementPage = 'https://demoqa.com/elements';
+        this.checkboxButton = page.locator('li:has-text("Check Box")');
+        this.urlCheckBox = 'https://demoqa.com/checkbox';
+        this.HomeLabel = page.getByLabel('Home');
+        this.DesktopLabel = page.getByLabel('Desktop');
+        this.NotesLabel = page.getByLabel('Notes');
+        this.CommandsLabel = page.getByLabel('Commands');
+        this.DocumentsLabel = page.getByLabel('Documents');
+        this.WorkspaceLabel = page.getByLabel('WorkSpace');
+        this.ReactLabel = page.getByLabel('React');
+        this.AngularLabel = page.getByLabel('React');
+        this.VeuLabel = page.getByLabel('React');
+        this.OfficeLabel = page.getByLabel('React');
+        this.PublicLabel = page.getByLabel('Public');
+        this.PrivateLabel = page.getByLabel('Private');
+        this.ClassifiedLabel = page.getByLabel('Classified');
+        this.GeneralLabel = page.getByLabel('General');
+        this.DownloadsLabel = page.getByLabel('Downloads');
+        this.WordFileLabel = page.getByLabel('Word File.doc');
+        this.ExcelFileLabel = page.getByLabel('Excel File.doc');
+        this.buttonExpandCheckBox = page.locator("//button[@title='Expand all']//*[name()='svg']//*[name()='path' and contains(@d,'M19 3H5c-1')]");
+        this.buttonCollapseCheckBox = page.locator("//button[@title='Collapse all']//*[name()='svg']//*[name()='path' and contains(@d,'M19 3H5c-1')]");
+    }
+
+    async gotoCheckBoxPage() {
+        await this.checkboxButton.click();
+    }
+
+    async expectSuccessGoToCheckBoxPage() {
+        await expect(this.page).toHaveURL(this.urlCheckBox);
+    }
+
+    async hitButtonCollapse() {
+        await this.buttonCollapseCheckBox.click();
+    }
+
+    async CollapseAllView() {
+        await expect(this.HomeLabel).toBeVisible();
+    }
+
+    async CollapseNotView() {
+        await expect(this.DesktopLabel).not.toBeVisible();
+    }
+
+    async clickHomeLabel(){
+        await this.HomeLabel.click();
+    }
+
+    async expectHomeLabelChecked(){
+        await expect(this.HomeLabel).toBeChecked();
+    }
+
 }
--- e2e/JavasScript/TestCase/DemoQA/DemoQA.spec.js
+++ e2e/JavasScript/TestCase/DemoQA/DemoQA.spec.js
@@ -1,27 +1,54 @@
-import { test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-import{
+import {
     GoToElementPageDemo,
-    TextBoxElementDemo
+    TextBoxElementDemo,
+    CheckBoxPage
 } from '../../Locator/DemoQA/DemoQA-Test'
 
-test.describe('DemoQA go to Web', { tag: '@demoQA'}, () => {
+test.describe('DemoQA go to Text Box', { tag: '@demoQA' }, () => {
     let webPage;
     let elementPage;
     test.beforeEach(async ({ page }) => {
         webPage = new GoToElementPageDemo(page);
-        await webPage.gotoElementPage();
-            
-    });
-
-    test('input all text box lalu submit', async({page}) => {
-
         elementPage = new TextBoxElementDemo(page);
-        await elementPage.expectSuccessGoToElementsPage();
+        await webPage.gotoElementPage();
 
-        await elementPage.toTextBoxPage();
-        await elementPage.inputDataInfo('John Doe','admin@admin.com','Earth','Mars');
-        await elementPage.hitButtonSubmit();
-        await elementPage.expectedResultSubmit('John Doe','admin@admin.com','Earth','Mars');
     });
+
+    test('input all text box lalu submit', async () => {
+        await elementPage.expectSuccessGoToElementsPage();
+        await elementPage.gotoTextBoxPage();
+        await elementPage.expectSuccessGoToTextBoxPage();
+        await elementPage.inputDataInfo('John Doe', 'admin@admin.com', 'Earth', 'Mars');
+        await elementPage.hitButtonSubmit();
+        await elementPage.expectedResultSubmit('John Doe', 'admin@admin.com', 'Earth', 'Mars');
+    });
+});
+
+test.describe('DemoQA go to CheckboxPage', { tag: '@demoQA' }, () => {
+    let webPage;
+    let checkBoxPage;
+    test.beforeEach(async ({ page }) => {
+        webPage = new GoToElementPageDemo(page);
+        checkBoxPage = new CheckBoxPage(page);
+        await webPage.gotoElementPage();
+    });
+
+    test('Testing Collapse Button', async () => {
+
+        await checkBoxPage.expectSuccessGoToElementsPage();
+        await checkBoxPage.gotoCheckBoxPage();
+        await checkBoxPage.expectSuccessGoToCheckBoxPage();
+        await checkBoxPage.hitButtonCollapse();
+        await checkBoxPage.CollapseAllView();
+        await checkBoxPage.CollapseNotView();
+    });
+
+    test('Home Label Checked', async () => {
+        await checkBoxPage.clickHomeLabel();
+        await checkBoxPage.expectHomeLabelChecked();
+    })
+
+
 });
